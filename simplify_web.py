9f4d11f659cbc6365ee2facdfea7e621640d7b46
gerak <gerice92@gmail.com>
2018-05-23 17:13:17 +0200

    [main] included crawler execution, text split in phrases, classifier, and generation of accessible web page

--- simplify_web.py
+++ simplify_web.py
@@ -4,19 +4,54 @@ from classifier.word_classifier import WordClassifier
 from synonyms.synonym_replace import SynonymReplace
 from website.web_generator import WebGenerator
 from website.web_launcher import WebLauncher
+from crawler.crawler.spiders.web_article_project import Article_crawler
+from scrapy.crawler import CrawlerProcess
+import webbrowser
+import json
+import os
 
-def main():
+def main(start_url):
+ 
+    #Lauch Crawler
+    CrawlingProcess = CrawlerProcess({
+        'FEED_URI': 'file:article.json',
+    })
+    try:
+        os.remove('article.json')
+    except OSError:
+        pass
+    CrawlingProcess.crawl(Article_crawler, start_url = start_url)
+    CrawlingProcess.start()
+
+    #read json
+    with open('article.json') as f:
+        data = json.load(f)
+        data_in_lines = data['text'].split(".")
+    print(data_in_lines)
+
+    #Init wordClassifier
     wc = WordClassifier()
     wc.train_classifier('naive-bayes')
     #wc.test_classifiers()
     new_sentences = list()
-    for line in ["this is a test of the complex word classifier, does it work strange thing huh?", "Evergreen trees are a symbol of fertility because they do not die in the winter"]:
+    #for line in ["this is a test of the complex word classifier, does it work strange thing huh?", "Evergreen trees are a symbol of fertility because they do not die in the winter"]:
+    for line in data_in_lines:
         wc_result = wc.classify(line)
         sr = SynonymReplace()
         sr_result = sr.word_swap(line, wc_result)
         new_sentences.append(sr_result)
     print(new_sentences)
-    return
+
+    title = data["title"]
+    img = data["img"]
+    text = new_sentences
+    article = [title,img,text]
+    
+    return article
 
 if __name__ == "__main__":
-    main()
+   article = main("https://mashable.com/2018/04/28/facebook-fake-news-smaller-feed/#JMBs_1D1Paqs")
+   print(article[0])
+   print(article[1])
+   print(article[2])
+   WebGenerator.webGenerator(article[0],article[1],article[2])
